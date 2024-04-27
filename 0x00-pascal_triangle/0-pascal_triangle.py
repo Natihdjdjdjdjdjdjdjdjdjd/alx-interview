cb9fbebe08b57725a2b1bfbfc6d402cc61eb3167
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-04-27 07:10:01 +0000

    0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+0-pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+
+    m = []
+    if n <= 0:
+        return m
+    m = [[1]]
+    for x in range(1, n):
+        temp = [1]
+        for j in range(len(m[x - 1]) - 1):
+            curr = m[x - 1]
+            temp.append(m[x - 1][y] + m[x - 1][y + 1])
+        temp.append(1)
+        m.append(temp)
+    return m
