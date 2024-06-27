cc38d41013e789d766727cd55d6adfa95d7ac194
Natihdjdjdjdjdjdjdjdjdjd <natasbest@gmail.com>
2024-06-27 19:05:23 +0000

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -3,19 +3,27 @@
 
 
 def island_perimeter(grid):
-    """Return the perimiter of an island.
+    """Returns the perimeter of the island describe
     """
-    w = len(grid[0])
-    h = len(grid)
-    edges = 0
-    size = 0
+    # Determine the number of rows and columns in the grid
+    my_rows = len(grid)
+    my_cols = len(grid[0])
 
-    for x in range(h):
-        for y in range(w):
-            if grid[x][y] == 1:
-                size += 1
-                if (y > 0 and grid[x][x - 1] == 1):
-                    edges += 1
-                if (x > 0 and grid[x - 1][y] == 1):
-                    edges += 1
-    return size * 4 - edges * 2
+    # Initialize the perimeter variable to 0
+    peri_meter = 0
+
+    # Loop through each cell in the grid
+    for i in range(my_rows):
+        for j in range(my_cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    peri_meter += 1
+                if i == my_rows-1 or grid[i+1][j] == 0:
+                    peri_meter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    peri_meter += 1
+                if j == my_cols-1 or grid[i][j+1] == 0:
+                    peri_meter += 1
+
+    # Return the total perimeter
+    return peri_meter
